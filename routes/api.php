7d7b2c5a1810ed94570352829116124612839c6a
Yazid Slimani <yazid.slimani@ensia.edu.dz>
2023-05-10 12:40:46 +0100

    same.

--- routes/api.php
+++ routes/api.php
@@ -22,8 +22,9 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post("/books/add", [BookController::class, 'AddBook']);
     Route::delete("/book/{id}", [BookController::class, 'DeleteBook']);
     Route::post("/book/{id}", [BookController::class, 'LoanBook']);
+    
 });
-
+Route::get("/books",[BookController::class,'AllBooks']);
 // authentication routes
 Route::post('/AddAdmin', [AuthController::class, 'signup']);
 Route::post('/AdminLogin', [AuthController::class, 'login']);
