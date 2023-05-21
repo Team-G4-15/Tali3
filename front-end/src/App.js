8664822a71a522adafbb5d522a5c8e946efb5a21
Hafida BOUKEDJAR <hafida.boukedjar@ensia.edu.dz>
2023-05-21 11:41:52 +0100

    fixed

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,65 +1,50 @@
 import { useState } from "react";
-import { Routes, Route } from "react-router-dom";
-import Topbar from "./scenes/global/Topbar";
-import Sidebar from "./scenes/global/Sidebar";
-import Dashboard from "./scenes/dashboard";
-import Team from "./scenes/team";
-import Bar from "./scenes/bar";
-import Form from "./scenes/form";
-import Line from "./scenes/line";
-import Pie from "./scenes/pie";
-import FAQ from "./scenes/faq";
-import Login from "./components/login.component";
-import SignUp from "./components/signup.component";
-import Geography from "./scenes/geography";
+import { Routes, Route, useLocation } from "react-router-dom";
+
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
-import Calendar from "./scenes/calendar/calendar";
 import { UserContextProvider } from "./contexts/UserContextProvider";
 import AddBook from "./components/Book.add";
-import SearchPage from "./scenes/Filtering";
-import PeriodicalSearch from "./scenes/Filtering/PeriodicalsSearch";
-import BookSearch from "./scenes/Filtering/BookSearch";
-import ResearchPapers from "./scenes/researchpapers";
-import AddResearchPaper from "./components/ResearchPaper.add";
-import Cataloging from "./scenes/Cataloging";
-function App() {
-  const [theme, colorMode] = useMode();
-  const [isSidebar, setIsSidebar] = useState(true);
+import SplitScreenLogin from "./components/SplitScreen.login";
+import SplitScreenSignup from "./components/SplitScreen.signup";
 
-  return (
-    <UserContextProvider>
-      <ColorModeContext.Provider value={colorMode}>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <div className="app">
-            <Sidebar isSidebar={isSidebar} />
-            <main className="content">
-              <Topbar setIsSidebar={setIsSidebar} />
-              <Routes>
-                <Route path="/" element={<SignUp />} />
-                <Route path="/team" element={<Team />} />
-                <Route path="/Cataliging" element={<Cataloging />} />
-                <Route path="/form" element={<Form />} />
-                <Route path="/bar" element={<Bar />} />
-                <Route path="/pie" element={<Pie />} />
-                <Route path="/line" element={<Line />} />
-                <Route path="/faq" element={<FAQ />} />
-                <Route path="/calendar" element={<Calendar />} />
-                <Route path="/geography" element={<Geography />} />
-                <Route path="/login" element={<Login />}></Route>
-                <Route path="/dashboard" element={<Dashboard />}></Route>
-                <Route path="/addBook" element={<AddBook />}></Route>
-                <Route path="/researchpapers/add" element={<AddResearchPaper />} />
-                <Route path="/Filtering" element={<SearchPage />}></Route>
-                <Route path="/researchpapers" element={<ResearchPapers/>}></Route>
-              </Routes>
-            </main>
-          </div>
-        </ThemeProvider>
-      </ColorModeContext.Provider>
-    </UserContextProvider>
-  );
+import { ProtectedRoutes } from "./protectedRoutes";
+function App() {
+    const [theme, colorMode] = useMode();
+    const [isSidebar, setIsSidebar] = useState(true);
+
+    const { pathname } = useLocation();
+
+    return (
+        <UserContextProvider>
+            <ColorModeContext.Provider value={colorMode}>
+                <ThemeProvider theme={theme}>
+                    <CssBaseline />
+                    <div className="app">
+                        {pathname === "/login" || pathname === "/" ? (
+                            <Routes>
+                                <Route
+                                    path="/"
+                                    element={<SplitScreenSignup />}
+                                />
+                                <Route
+                                    path="/login"
+                                    element={<SplitScreenLogin />}
+                                />
+                            </Routes>
+                        ) : (
+                            <>
+                                <ProtectedRoutes
+                                    isSidebar={isSidebar}
+                                    setIsSidebar={setIsSidebar}
+                                ></ProtectedRoutes>
+                            </>
+                        )}
+                    </div>
+                </ThemeProvider>
+            </ColorModeContext.Provider>
+        </UserContextProvider>
+    );
 }
 
 export default App;
